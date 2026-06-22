1354479bf252f8004881860773f7f9f4bf982601
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-06-22 22:12:25 +0200

    Added basic UI overlay box

--- app/src/main/java/com/gruppe5/roguelike/MainActivity.kt
+++ app/src/main/java/com/gruppe5/roguelike/MainActivity.kt
@@ -11,12 +11,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -27,6 +29,7 @@ import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -68,6 +71,7 @@ fun MainScreen(modifier: Modifier = Modifier, model: RoguelikeViewModel = viewMo
     val screenWidth = windowInfo.containerDpSize.width
     val screenHeight = windowInfo.containerDpSize.height
 
+    // Map Container Box
     Box(modifier = modifier
         .pointerInput(Unit) {
             val boxSize = this.size
@@ -131,6 +135,18 @@ fun MainScreen(modifier: Modifier = Modifier, model: RoguelikeViewModel = viewMo
             }
         }
 
+    }
+
+    // UI Overlay Box
+    Box(modifier = modifier.padding(16.dp)) {
+        Row {
+            Text(
+                text = "$turn",
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth(),
+                textAlign = TextAlign.Right
+            )
+        }
     }
 
 }
