3ac8b8902255351b81d95cc3d4d15fffd4bbcec4
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2026-07-10 02:10:11 +0200

    Added coroutines dependency
    Will definitely come in handy later

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.kotlinx.coroutines.core)
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.kotlinx.serialization.json)
